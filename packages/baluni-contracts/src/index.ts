88d49f7bae3c19a6ae251b85ddd78f36e618a75d
SimoneG <info@apperside.com>
2024-07-18 16:05:48 +0200

    consume json in baluni backend exported from baluni contracts

--- packages/baluni-contracts/src/index.ts
+++ packages/baluni-contracts/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/baluni-contracts';
+
+import * as testContract from '../artifacts/contracts/registry/BaluniV1YearnVaultRegistry.sol/BaluniV1YearnVaultRegistry.json';
+
+export { testContract };
